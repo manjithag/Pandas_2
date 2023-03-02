c6b991cae4e56ec1776cbcee3a27c0f9ad2fe13c
Manjitha <manjitha.ee@gmail.com>
2023-03-02 12:53:40 +0100

    commit 3

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 ## Sorting & Describing Data
 
 import pandas as pd
+df = pd.read_csv('pokemon_data.csv')
+
+print("---------Print 1-------------")
+print(df.describe())
+
+print("---------Print 2-------------")
+print(df.sort_values(['Name']))
+
+print("---------Print 3-------------")
+print(df.sort_values('Name', ascending=False))
+
+print("---------Print 4-------------")
+print(df.sort_values(['Name', 'Type 1']))
